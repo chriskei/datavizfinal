9d0fd16289aa460ee9e7bf46b9c7931b8a13cd33
Christopher Kei <ckei@Christophers-MacBook-Air.local>
2022-04-08 12:48:40 -0400

    add command line argument check

--- spotipy-test.py
+++ spotipy-test.py
@@ -1,8 +1,15 @@
+import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-my_playlist = sp.playlist('3FACwN2Ta8kXFcbquQ2u6K')
+# Make sure the user enters a playlist id
+if len(sys.argv) != 2:
+    print("Please add a playlist id as an argument when running")
+    sys.exit()
+
+playlist_id = sys.argv[1] # Test playlist id: 3FACwN2Ta8kXFcbquQ2u6K 
+my_playlist = sp.playlist(playlist_id)
 print(my_playlist)
