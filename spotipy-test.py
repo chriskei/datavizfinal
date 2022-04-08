6bb9f88547c7ba075472244cbc0fd3a5c32ce3a1
Julian L <jlechner45@gmail.com>
2022-04-08 13:00:40 -0400

    added parse_args

--- spotipy-test.py
+++ spotipy-test.py
@@ -2,14 +2,22 @@ import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-auth_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(auth_manager=auth_manager)
+def parse_args():
+    # Make sure the user enters a playlist id
+    if len(sys.argv) != 2:
+        print("Please add a playlist id as an argument when running")
+        sys.exit()
 
-# Make sure the user enters a playlist id
-if len(sys.argv) != 2:
-    print("Please add a playlist id as an argument when running")
-    sys.exit()
+    playlist_id = sys.argv[1] # Test playlist id: 3FACwN2Ta8kXFcbquQ2u6K 
+    
+    return playlist_id
 
-playlist_id = sys.argv[1] # Test playlist id: 3FACwN2Ta8kXFcbquQ2u6K 
-my_playlist = sp.playlist(playlist_id)
-print(my_playlist)
+
+if __name__ == "__main__":
+    playlist_id = parse_args()
+    
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    my_playlist = sp.playlist(playlist_id)
+    print(my_playlist)
